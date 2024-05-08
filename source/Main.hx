4de402e54cfabc40e797f28ee6b474ce17ada244
h4master <107293097+ahika462@users.noreply.github.com>
2024-05-09 01:09:37 +1100

    funny crashes

--- source/Main.hx
+++ source/Main.hx
@@ -1,10 +1,29 @@
+import haxe.CallStack;
+import openfl.events.UncaughtErrorEvent;
+import openfl.Lib;
 import openfl.display.FPS;
 import flixel.FlxG;
 import flixel.FlxGame;
 
 class Main extends FlxGame {
 	function new() {
+		#if (hl && !debug)
+		hl.UI.closeConsole();
+		#end
+
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onFatalUncaughtException);
+
 		super(TitleState);
 		FlxG.stage.addChild(new FPS(10, 3, 0xffffffff));
+	}
+
+	static function onFatalUncaughtException(e:UncaughtErrorEvent) {
+		var error = e.error;
+
+		var exception = CallStack.toString(CallStack.exceptionStack(true));
+		error += '\n\n$exception';
+	
+		FlxG.stage.window.alert(error, 'Fatal Uncaught Exception!');
+		Sys.exit(1);
 	}
 }
